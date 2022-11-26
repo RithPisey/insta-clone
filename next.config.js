bcb3206756880d506583de7e9a73f2a4cbc53fb6
Rithpisey <hengrithpisey@gmail.com>
2022-11-26 23:21:27 +0700

    config next.config.js->domains

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ["https://upload.wikimedia.org/"],
+		domains: ["upload.wikimedia.org"],
 		remotePatterns: [
 			{
 				protocol: "https",
